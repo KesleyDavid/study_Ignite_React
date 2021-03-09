7fde4b5054d41e9773e218227925c3c153d308c4
Kesley David <kesleygti@gmail.com>
2021-03-09 12:32:07 -0300

    [01] [project] Import files css

--- web/01-github-explorer/src/App.jsx
+++ web/01-github-explorer/src/App.jsx
@@ -1,6 +1,8 @@
+import './styles/global.css';
+
 export function App() {
 
-  throw new Error('Ops, gerei um erro');
-  
+  // throw new Error('Ops, gerei um erro');
+
   return <h1>Kesley David</h1>
 }
--- web/01-github-explorer/webpack.config.js
+++ web/01-github-explorer/webpack.config.js
@@ -31,6 +31,11 @@ module.exports = {
         // \ => antes do ponto -> o ponto indiga qualquer caracter, mas queremos o ponto (.)
         exclude: /node_modules/,
         use: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: ['style-loader','css-loader'],
       }
     ]
   }
